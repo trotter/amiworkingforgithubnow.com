957449d0c21f693fded24951265962c5a584ce39
Lee Reilly <lee@leereilly.net>
2011-09-06 19:03:18 -0700

    Added support for hotlinks e.g. http://amiworkingforgithubnow.com/leereilly

--- amiworkingforgithubnow.rb
+++ amiworkingforgithubnow.rb
@@ -10,15 +10,16 @@ get '/' do
   erb :index
 end
 
+get '/:handle/?' do
+  if is_working_at_github?(params[:handle])
+    erb :yes
+  else
+    erb :no
+  end
+end
+
 post '/working' do
-  req = Net::HTTP::Get.new("/api/v2/json/user/show/#{params[:handle].downcase}/organizations")
-
-  http = Net::HTTP.new("github.com", 443)
-  http.use_ssl = true
-
-  myorgs = JSON.parse(http.request(req).body)["organizations"]
-
-  if myorgs.detect {|org| org["name"].eql?("GitHub")}
+  if is_working_at_github?(params[:handle])
     erb :yes
   else
     erb :no
@@ -31,4 +32,19 @@ end
 
 get '/no' do
   erb :no
+end
+
+def is_working_at_github?(handle)
+  req = Net::HTTP::Get.new("/api/v2/json/user/show/#{handle.downcase}/organizations")
+
+  http = Net::HTTP.new("github.com", 443)
+  http.use_ssl = true
+
+  myorgs = JSON.parse(http.request(req).body)["organizations"]
+
+  if myorgs.detect {|org| org["name"].eql?("GitHub")}
+    return true
+  else
+    return false
+  end
 end
